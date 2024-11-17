717f96a820f0accc42fa836da58de47f01e58d13
Piroghok <piroghok121233@gmail.com>
2024-11-17 20:43:26 +0200

    errors formating develop

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    return {"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    return {
+        "errors":
+            [
+                {"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
+                for error in errors
+            ],
+        "path": file_path, "status": "failed"
+    }
+
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    return [
+        {"errors":
+             [{"line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"}
+                 for error in errors],
+        "path": file,
+        "status": "failed" if errors else "passed"
+        }
+        for file, errors in linter_report.items()]
     pass
